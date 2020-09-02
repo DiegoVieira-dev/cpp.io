4e34427bf5ea1d5a2d73fe22174be9221301b400
Diego Vieira <diegow.contato@gmail.com>
2020-09-02 13:24:43 -0300

    Update Exc.2.30.cpp
    Longe de finalizar!!!

--- Exc.2.30.cpp
+++ Exc.2.30.cpp
@@ -6,17 +6,77 @@
  * + 0 * 2 + 1 * 4 +1 * 8 ou 13).*/
 
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
- int main(){	
-	 
-	 int n;
-	 
-	 cout<<"Forneça um digito binario: ";
-	 cin>>n;
-	 
-	 
-	 
-	 return 0;
-	 }
+int main() {
+	
+	int bin,i = 0, a, b, c, d, e, f, g, h, j;
+	
+	cout<<"Digite um byte qualquer: ";
+	cin>>bin;
+	while(i<8,i++){
+		
+		if(i==0){
+			a = (bin/pow(10,8));
+			if(a!=0){
+				a = 1;
+			}
+		}else{
+			if(i==1){
+				b = (bin/pow(10,7));
+				if(b!=0){
+					b = 2;
+				}
+			}else{
+				if(i==2){
+					c = (bin/pow(10,6));
+					if(c!=0){
+						c = 3;
+					}
+				}else{
+					if(i==3){
+						d = (bin/pow(10,5));
+						if(d!=0){
+							d = 4;
+						}
+					}else{
+						if(i==4){
+							e = (bin/pow(10,4));
+							if(e!=0){
+								e = 5;
+							}
+						}else{
+							if(i==5){
+								f = (bin/pow(10,3));
+								if(f!=0){
+									f = 6;
+								}
+							}else{
+								if(i==6){
+									g = (bin/pow(10,2));
+									if(g!=0){
+										g = 7;
+									}
+								}else{
+									if(i==7){
+										h = (bin/pow(10,1));
+										if(h!=0){
+											h = 8;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+				}
+			}
+			
+	}
+	
+	cout<<g ;
+	
+	return 0;
+}
