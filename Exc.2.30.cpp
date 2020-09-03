d4ba09f98672c21a864eb8ba5093eb651a30a18d
Diego Vieira <diegow.contato@gmail.com>
2020-09-02 21:08:04 -0300

    Update Exc.2.30.cpp

--- Exc.2.30.cpp
+++ Exc.2.30.cpp
@@ -18,9 +18,9 @@ int main() {
 	
 	a = (n/10000000);
 	b = ((n/1000000) - a *10);
-        c = (((n/100000) - a * 100) - b * 10);
-        d = ((((n/10000) -a * 1000)- b * 100)-c * 10);
-        e = (((((n/1000) - a * 10000)- b * 1000)-c*100)-d * 10); 
+    c = (((n/100000) - a * 100) - b * 10);
+    d = ((((n/10000) -a * 1000)- b * 100)-c * 10);
+    e = (((((n/1000) - a * 10000)- b * 1000)-c*100)-d * 10); 
 	f = ((((((n/100) - a *100000)- b * 10000) - c * 1000) - d * 100) - e * 10);
 	g = (((((((n/10) - a * 1000000)- b * 100000)- c * 10000)- d * 1000)- e *100) - f *10); 
 	h = (((((((n - a * 10000000)- b * 1000000)- c * 100000)- d * 10000)- e * 1000) - f * 100)- g * 10);
@@ -49,20 +49,25 @@ int main() {
 	if(h!=0 && h==1){
 		h = 1;
 	}else{
-	if(a>1 && b>1 && c>1 && d>1 && e>1 && f>1 && g>1 && h>1 && n>8 ){
+	if(a>1 && b>1 && c>1 && d>1 && e>1 && f>1 && g>1 && h>1  ){
 			
 			cout<<"\n\nDigite um byte ou tente de novo!\n\n";	
 			
 	system("pause");
-		}else{
-		
-		total = a + b + c + d + e + f + g + h;	
-	
-		cout<<"\n\nEsse byte equivale ao numero: "<< total << endl;
-	
+		}
 	}
-    }
 	
+	total = a + b + c + d + e + f + g + h;	
+	
+	if (n>1){
+		
+		cout<<"\n\nEsse byte equivale ao numero: "<< total << endl;
+		
+	}
+
+	system("pause");
+	
+    
 	
 	return 0;
 }
